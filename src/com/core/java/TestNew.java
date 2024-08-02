6a194d689963aac6adf0224298339ca394ea2cc5
nagaraju_kailasa <nagaraju.kailasa123@gmail.com>
2024-08-02 18:00:02 -0400

    modified files and removed files

--- src/com/core/java/TestNew.java
+++ src/com/core/java/TestNew.java
@@ -1,22 +1,8 @@
 package com.core.java;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class TestNew {
 
 	public static void main(String[] args) {
-		String str = "how are you doing today";
-		char[] ch = str.toCharArray();
-
-		// Map<Character, Integer> map = new HashMap<>();
-		Set set = new HashSet();
-		for (char c : ch) {
-			if (set.contains(c)) {
-				set.add(c);
-			}
-			System.out.println(set);
-
-		}
+		
 	}
 }
