63f5e5ba47ed60155029b44c9c98ffcecd0c495e
nagaraju_kailasa <nagaraju.kailasa123@gmail.com>
2024-08-25 01:44:47 +0530

    incorrect result with the main and child thread executing at a time for factorial computation

--- src/com/core/java/ManualThreadsCreationMain.java
+++ src/com/core/java/ManualThreadsCreationMain.java
@@ -5,9 +5,17 @@ public class ManualThreadsCreationMain {
 	public static void main(String[] args) {
 		long startTime = System.currentTimeMillis();
 		for(int i=1; i<10; i++) {
-			System.out.println(factorial(i));
+			//now using multi threading
+			int finalI = i;
+			Thread thread = new Thread(
+					()->{
+						long result = factorial(finalI);
+						System.out.println(result);
+					}
+				);
+			thread.start(); //here, one thread is created along with main thread
 		}
-		System.out.println("total time taken for execution : " + (System.currentTimeMillis() - startTime));
+		System.out.println("total time taken for execution : " + (System.currentTimeMillis() - startTime)); //main is not waiting because we won't call join method on thread so we got incorrect result
 	}
 	
 	private static long factorial(int n) {
