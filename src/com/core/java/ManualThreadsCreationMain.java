1032095a410403b1a840f55124781fc38206a618
nagaraju_kailasa <nagaraju.kailasa123@gmail.com>
2024-08-25 02:13:56 +0530

    using executorservice with 3 threads

--- src/com/core/java/ManualThreadsCreationMain.java
+++ src/com/core/java/ManualThreadsCreationMain.java
@@ -1,40 +1,37 @@
 package com.core.java;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class ManualThreadsCreationMain {
 
 	public static void main(String[] args) {
 		long startTime = System.currentTimeMillis();
-		Thread[] threads = new Thread[9];
+		//Thread[] threads = new Thread[9]; //here 9 locations for 9 threads creation
+		ExecutorService executorService = Executors.newFixedThreadPool(3); //here we are creating only 3 threads those are reusing to achieve the task
 		for(int i=1; i<10; i++) {
-			//now using multi threading
 			int finalI = i;
-			threads[i-1] = new Thread(
-					()->{
+			//threads[i-1] = new Thread( //threads are creating manually one by one and not reusing here
+			executorService.submit(()->{
 						long result = factorial(finalI);
 						System.out.println(result);
-					}
-				);
-			threads[i-1].start(); //10 threads along with main
+					});
 		}
 		
-		for(Thread thread : threads) {
-			try {
-				thread.join();
-			} catch (InterruptedException e) {
-				Thread.currentThread().interrupt();
-			}
+		executorService.shutdown(); //if not shutdown, it will keep on using
+		try {
+			executorService.awaitTermination(100, TimeUnit.SECONDS); //main should not wait if not write this statement 
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
-		System.out.println("total time taken for execution : " + (System.currentTimeMillis() - startTime)); //now main thread should wait until completion of all child threads and now we will get correct result
-		//without multi threading the time consumed for the execution is 9 seconds
-		//with multi threading  the time consumed for the execution is 1 second
-		//this is manual thread creation and execution
+		System.out.println("total time taken for execution : " + (System.currentTimeMillis() - startTime)); //this line will execute first if we don't call awaitTermination method on executerService 
 	}
 	
-	private static long factorial(int n) {
+	private static long factorial(int n) { //factorial logic
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		int result = 1;
